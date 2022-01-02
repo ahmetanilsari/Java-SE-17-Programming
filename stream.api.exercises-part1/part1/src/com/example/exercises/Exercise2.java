ecd0fce5eb11a49849a3e82f2dac9591ea75ddf0
Ahmet Anıl <72103632+ahmetanilsari@users.noreply.github.com>
2022-01-02 17:09:07 +0300

    Add files via upload

--- stream.api.exercises-part1/part1/src/com/example/exercises/Exercise2.java
+++ stream.api.exercises-part1/part1/src/com/example/exercises/Exercise2.java
@@ -1,7 +1,14 @@
 package com.example.exercises;
 
+import java.util.Collection;
+import java.util.Map;
+import java.util.stream.Collectors;
+
 import com.example.dao.CountryDao;
 import com.example.dao.InMemoryWorldDao;
+import com.example.domain.City;
+import com.example.domain.Country;
+import com.example.domain.Director;
 
 /**
  * 
@@ -13,6 +20,14 @@ public class Exercise2 {
 
 	public static void main(String[] args) {
 		// Find the most populated city of each continent
+		var highPopulatedCityOfEachContinent = 
+				countryDao.findAllCountries()
+				          .stream()
+				          .map( country -> country.getCities().stream().map(city -> new ContinentCityPair(country.getContinent(),city)).toList())
+				          .flatMap(Collection::stream)
+				          .collect(Collectors.groupingBy(ContinentCityPair::continent,Collectors.maxBy( (p1,p2) -> p1.city().getPopulation()-p2.city().getPopulation())));
+		highPopulatedCityOfEachContinent.forEach((continent,city)->System.out.printf("%s: %s\n",continent,city.get()));
+				 
 	}
 
 }
