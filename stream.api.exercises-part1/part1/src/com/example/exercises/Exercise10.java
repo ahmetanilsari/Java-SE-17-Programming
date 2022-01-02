ff47ba360f0c2bf0c0d3a32589659285234578e7
Ahmet Anıl <72103632+ahmetanilsari@users.noreply.github.com>
2022-01-02 19:46:47 +0300

    Add files via upload

--- stream.api.exercises-part1/part1/src/com/example/exercises/Exercise10.java
+++ stream.api.exercises-part1/part1/src/com/example/exercises/Exercise10.java
@@ -1,7 +1,11 @@
 package com.example.exercises;
 
+import java.util.Optional;
+import java.util.function.BiConsumer;
+
 import com.example.dao.InMemoryWorldDao;
 import com.example.dao.WorldDao;
+import com.example.domain.Country;
 
 /**
  * 
@@ -10,10 +14,15 @@ import com.example.dao.WorldDao;
  */
 public class Exercise10 {
 	private static final WorldDao worldDao = InMemoryWorldDao.getInstance();
+	private static final BiConsumer<String,Optional<Country>> printEntry = (continent,country) -> System.out.printf("%s: %s\n",continent,country.get());
 		
 	public static void main(String[] args) {
 		// Find the richest country of each continent with respect to their GNP (Gross National Product) values.
-
+		var richCountryCont = 
+				worldDao.findAllCountries()
+						.stream()
+						.collect(groupingBy(Country::getContinent,maxBy(comparing(Country::getGnp))));
+		richCountryCont.forEach(printEntry);
 	}
 
 }
