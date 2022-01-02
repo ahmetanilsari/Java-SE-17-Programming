194b93a600d364ee2e530c9d44f4dba48bd46f8f
Ahmet Anıl <72103632+ahmetanilsari@users.noreply.github.com>
2022-01-02 16:37:57 +0300

    Add files via upload

--- stream.api.exercises-part1/part1/src/com/example/exercises/Exercise3.java
+++ stream.api.exercises-part1/part1/src/com/example/exercises/Exercise3.java
@@ -1,5 +1,11 @@
 package com.example.exercises;
 
+import java.util.Collection;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+import com.example.domain.Genre;
+import com.example.domain.Movie;
 import com.example.service.InMemoryMovieService;
 import com.example.service.MovieService;
 
@@ -12,7 +18,15 @@ public class Exercise3 {
 	private static final MovieService movieService = InMemoryMovieService.getInstance();
 
 	public static void main(String[] args) {
-		// Find the number of genres of each director's movies
+		final Collection<Movie> genre = movieService.findAllMovies();
+		Map<String, Long> genreDirCount =
+				genre.stream().map(Movie::getGenres)
+							  .flatMap(Collection::stream)
+							  .collect(Collectors.groupingBy(Genre::getName, Collectors.counting())
+									  );
+		genreDirCount.forEach(
+        		(name,count) -> System.out.printf("%20s: %3d\n",name,count));
+				
 
 	}
 
